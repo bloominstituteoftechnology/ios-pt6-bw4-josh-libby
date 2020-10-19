a0cb50882b9fb425622f1b9af1b80096d1ec1d53
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-10-19 11:17:36 -0500

    Added Core Data along with getting the folders setup and Controllers connected to their views. Also added an image to the launch screen.

---
+++ NoteTranscription/View Controllers/AudioMemoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AudioMemoViewController.swift
+//  NoteTranscription
+//
+//  Created by Josh Kocsis on 10/19/20.
+//
+
+import UIKit
+
+class AudioMemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ NoteTranscription/View Controllers/NotesDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NotesDetailViewController.swift
+//  NoteTranscription
+//
+//  Created by Josh Kocsis on 10/19/20.
+//
+
+import UIKit
+
+class NotesDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
