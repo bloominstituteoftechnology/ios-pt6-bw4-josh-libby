fb759429c63dfc8a403101aa5f35c8376612daeb
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-10-23 16:12:01 -0500

    Lets try this one more time.

---
+++ NoteTranscription/View Controllers/AudioMemoTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  AudioMemoTableViewCell.swift
+//  NoteTranscription
+//
+//  Created by Josh Kocsis on 10/23/20.
+//
+
+import UIKit
+import AVFoundation
+
+class AudioMemoTableViewCell: UITableViewCell {
+
+    var recordingURL: URL?
+    var audioPlayer: AVAudioPlayer?
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    func prepareAudioSession() throws {
+        let session = AVAudioSession.sharedInstance()
+        try session.setCategory(.playback, options: .defaultToSpeaker)
+        try session.setActive(true, options: [])
+    }
+
+    @IBAction func playButtonTapped(_ sender: Any) {
+        do {
+            if let recordingURL = recordingURL {
+                audioPlayer = try AVAudioPlayer(contentsOf: recordingURL)
+                guard let audioPlayer = self.audioPlayer else { return }
+                audioPlayer.play()
+            }
+        } catch {
+            preconditionFailure("Failure to load audio file at path \(recordingURL!): \(error)")
+        }
+    }
+}
