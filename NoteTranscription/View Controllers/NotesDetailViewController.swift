36b16495c4639a8ae4150f6d0a81e5b862ed96be
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-10-23 15:40:57 -0500

    Added files that libby gave me as we are having trouble with git hub.

--- NoteTranscription/View Controllers/NotesDetailViewController.swift
+++ NoteTranscription/View Controllers/NotesDetailViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class NotesDetailViewController: UIViewController {
+    // MARK: - Properties
+
+    var note: Notes?
+
+    // MARK: - IBOutlets
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var bodyTextView: UITextView!
+
+    // MARK: - View Live Cycles
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
+    }
+
+    // MARK: - Functions
+
+    func updateViews() {
+        guard let note = note, let image = UIImage(data: note.img) else { return }
+
+        imageView.image = image
+        titleTextField.text = note.title
+        bodyTextView.text = note.bodyText
 
     }
 }
